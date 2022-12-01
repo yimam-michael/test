c2ee50c2e0b725ac9b58ef641cb999b66c196bfa
Michael Yimam <michaelyimam12@gmail.com>
2022-11-30 19:58:59 -0600

    stuff...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumComponent } from './album/album/album.component';
+import { CatFactComponent } from './cat-facts/cat-fact/cat-fact.component';
+import { UsersComponent } from './users/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cat-fact', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'cat-fact', component: CatFactComponent },
+  { path: 'album', component: AlbumComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
